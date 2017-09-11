eb7a60a68c6a57a680b94e0ecc759a2b13870a7f
jcoder <106833215@qq.com>
2017-09-11 23:09:54 +0800

    【新增】初始化提交

---
+++ pro-web/src/main/java/com/alm/commonpro/WebProApplication.java
@@ -0,0 +1,12 @@
+package com.alm.commonpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProApplication.class, args);
+    }
+}
